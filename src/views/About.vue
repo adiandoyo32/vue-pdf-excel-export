1de68b8e7b9523d87191feea38941f96c4052c19
akhiang <akhiang32@gmail.com>
2021-09-22 00:44:30 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+      <!-- <pdf src="@/assets/document.pdf"></pdf> -->
+      <!-- <img src="@/assets/logo.png" alt=""> -->
+      <input type="number" v-model.number="page"> /{{numPages}}
+    <pdf src="@/assets/document.pdf" 
+      :page="page" 
+      @num-pages="numPages = $event" 
+      style="width:75%"></pdf>
   </div>
 </template>
+
+<script>
+import pdf from 'vue-pdf'
+
+export default {
+  components: {
+    pdf
+  },
+  data() {
+    return {
+      page: 1,
+      numPages: 1,
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+
+        <table ref="table">
+            <thead>
+                <tr>
+                    <th rowspan="2" class="text-center">Tanggal</th>
+                    <th rowspan="2" class="text-center">No. Bukti</th>
+                    <th>Supplier</th>
+                    <th colspan="2" class="text-right">Bruto</th>
+                    <th class="text-right">Disc</th>
+                    <th class="text-right">PPN</th>
+                    <th rowspan="2" class="text-right">Netto</th>
+                </tr>
+                <tr>
+                    <th>Nama Barang</th>
+                    <th class="text-center">Kuantitas</th>
+                    <th class="text-right">Harga</th>
+                    <th class="text-right">Disc. Item</th>
+                    <th class="text-center">Panjang</th>
+                </tr>
+            </thead>
+            <tbody ref="body">
+                <tr>
+                    <td rowspan="2">2021-09-20</td>
+                    <td rowspan="2">112112</td>
+                    <td>Amin</td>
+                    <td colspan="2" data-type="asd">40.000,002</td>
+                    <td>0</td>
+                    <td>4,000</td>
+                    <td rowspan="2">4.0000</td>
+                </tr>
+                <tr>
+                    <td>coil</td>
+                    <td>4</td>
+                    <td>10.000</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <button type="button" v-on:click="onexport">Excel download</button>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import XLSX from "xlsx";
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    components: {
+        HelloWorld,
+    },
+    data: () => ({}),
+    methods: {
+        onexport() {
+            var table = this.$refs.table;
+            this.$refs.body.children.forEach((element) => {
+                element.children.forEach((child, index) => {
+                    if (child.dataset.type) {
+                        console.log(child, child.dataset.type);
+                        // var number = parseFloat(child.innerHTML);
+                        child.innerHTML = child.innerHTML.replace(/\./g, ',').replace(/\,/g, '.')
+                    }
+                    // console.log(parseFloat(child.innerHTML))
+                    // console.log(child)
+                });
+            });
+            // console.log(this.$refs.body.children[0].children[0].innerHTML = 'asd')
+            // table.innerHTML = "aasdasd"
+            // var wb = XLSX.utils.table_to_book(table);
+
+            //  XLSX.writeFile(wb, 'book.xlsx')
+        },
+    },
+};
 </script>
+
+<style scoped>
+table {
+    border: 1px solid black;
+}
+
+table th,
+table td {
+    border: 1px solid black;
+}
+</style>
